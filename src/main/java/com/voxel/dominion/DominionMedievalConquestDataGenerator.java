02b77f92fb779b5e1d7f5cbc8a7d057723949833
wilhelmstander24-tech <wilhelmstander24@gmail.com>
2026-02-16 19:56:37 +0200

    Setup

---
+++ src/main/java/com/voxel/dominion/DominionMedievalConquestDataGenerator.java
@@ -0,0 +1,11 @@
+package com.voxel.dominion;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DominionMedievalConquestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
